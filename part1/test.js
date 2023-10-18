416ca7960be9668b8c44f801746f935260f4121e
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-18 18:47:31 +0000

    hello2

--- part1/test.js
+++ part1/test.js
@@ -1,1 +1,15 @@
 console.log("Hemant");
+const nameStd= "Rahul";
+let rollNumber="20bce11002";
+var applicatNo= 2547;
+markSheet=100;
+
+console.log(nameStd);
+console.log(rollNumber);
+console.log(applicatNo);
+console.log(markSheet);
+let strType= typeof markSheet;
+console.log(typeof(markSheet));
+console.table([nameStd,rollNumber, applicatNo, markSheet,strType]);
+
+/* Try not to use var because of issue in block scope and function scope */ 
