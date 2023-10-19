1531a650a07deb4285086b341567382b7ee54687
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 09:49:35 +0000

    Stack and heap

---
+++ part1/stackandHeap.js
@@ -0,0 +1,22 @@
+// let myname= "mynameishemant";
+// let another = myname;
+// console.log(another);
+// another = " friends";
+// console.log(another);
+// console.log(myname);
+
+// Non primitive
+
+let userone = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
+}
+let usertwo = userone;
+console.log(userone.email);
+console.log(userone.upi);
+usertwo.email="hemant@gmail";
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
