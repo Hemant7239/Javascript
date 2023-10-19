d50ebd7264280442f13d57db731bec6821a5ee8b
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 13:39:50 +0000

    Date and time

---
+++ part1/dateandtime.js
@@ -0,0 +1,27 @@
+// Date
+// let myDate = new Date();
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// let myDate = new Date(2023, 0, 23)
+// console.log(myDate.toDateString());
+
+ let myDate = new Date("01-14-2023");
+// console.log(myDate.toLocaleString());
+
+// let myTimeStamp = Date.now();          //today time in millisec form 1 jan 1970;
+// console.log(myTimeStamp);
+// console.log(myDate.getTime());
+// console.log(Math.floor(Date.now()/1000));     // covert in second
+
+
+let newDate = new Date();
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
