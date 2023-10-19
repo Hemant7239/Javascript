a5b35ac455ae376f0a6823b888a7767603231a0c
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 14:52:18 +0000

    Array1

---
+++ part1/arrays.js
@@ -0,0 +1,26 @@
+// const arr = [1, 2, 6, 5, 4, 9];
+// console.log(arr[2]);
+//  const myarr = new Array(1, 2, 3, 4);
+//  console.log(myarr[2]);
+
+//  myarr.push(6);
+//  myarr.push(9);
+//  console.log(myarr);
+//  myarr.pop();
+//  console.log(myarr);
+//  myarr.unshift(5);
+//  console.log(myarr);
+//  myarr.shift(5);
+//  console.log(myarr);
+
+const arr = new Array(1, 2, 3, 4, 5);
+console.log(arr);
+const myn1 = arr.slice(1,3);
+console.log(myn1);
+console.log(arr);
+const myn2 = arr.splice(1,3);
+console.log(myn2);
+console.log(arr);
+
+
+
