c2e23e8c75c384ced44baae4b96b9735ba7ed7f8
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 05:55:30 +0000

    comparison

---
+++ part1/04_comparison.js
@@ -0,0 +1,12 @@
+// comparison;
+console.log("6">5);
+console.log("4"==4);
+console.log(null==0);
+console.log("6"===6);
+console.log("6">=5);
+console.log("8"<=7);
+console.log(null >= 0);
+
+console.log(undefined==0);
+console.log(undefined>0);
+console.log(undefined>=0);
