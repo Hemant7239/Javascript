b7a4d1aa9a1bf29a3cae4e0bc2540a9aa02b0be1
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 04:22:29 +0000

    Conversion of datatype

---
+++ part1/test3.js
@@ -0,0 +1,37 @@
+//conversion from one datatype to another
+// number conversion 
+
+// let num = "33";
+// let num1 = "Hemant";
+// let num2= "33abc";
+// let num3 = null;
+// let num4 = true;
+// let con1= Number(num);
+// let con2= Number(num2);
+// let con3= Number(num3);
+// let con4= Number(num4);
+// //let con1= Number(num);
+// console.table([num,num1,num2,num3,num4]);
+// console.table([con1,con2,con3,con4]);
+
+// Boolean datatype 
+
+// let num = 0;
+// let boolnum1= Boolean(num);
+// let num1 = null;
+// let boolnum2= Boolean(num1);
+// console.log(boolnum1);
+// console.log(boolnum2);
+
+// String conversion
+
+let num = 0;
+let boolnum1= String(num);
+let num1 = null;
+let boolnum2= String(num1);
+console.log(boolnum1);
+console.log(boolnum2);
+
+
+
+
