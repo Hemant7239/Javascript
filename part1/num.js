9498db15b77a7129253b844df216da05734779b7
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 12:46:22 +0000

    Number and maths

---
+++ part1/num.js
@@ -0,0 +1,21 @@
+// const score = 100;
+// console.log(score);
+// const balance = new Number(200);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+// const count= 100.525;
+// console.log(count.toPrecision(3));
+// const rupee= 10000000;
+// console.log(rupee.toLocaleString('en-IN'));
+
+//   MATHS
+
+console.log(Math);
+console.log(Math.abs(-13));
+console.log(Math.round(4.6));
+console.log(Math.round(4.4));
+console.log(Math.ceil(4.4))
+console.log(Math.floor(4.8));
+console.log(Math.random());
+co
