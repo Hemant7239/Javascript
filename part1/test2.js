20fc1b30a222f55036e3a43e222fc157a7cd8ba7
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-19 03:31:59 +0000

    hello3

---
+++ part1/test2.js
@@ -0,0 +1,14 @@
+// Datatype 
+let namee = "Hemant kumar yadav";
+let age = 21;
+let id = 11002;
+let male = true;
+let place = null;
+let birth = undefined;
+console.log(namee);
+console.log(age);
+console.log(id);
+console.log(male);
+console.log( typeof place);
+console.log(birth);
+console.log(typeof birth);
