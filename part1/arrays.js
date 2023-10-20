b589f2d4459f2d88f6c017b56e3065d951965b1c
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-20 03:10:41 +0000

    Array

--- part1/arrays.js
+++ part1/arrays.js
@@ -13,14 +13,33 @@
 //  myarr.shift(5);
 //  console.log(myarr);
 
-const arr = new Array(1, 2, 3, 4, 5);
-console.log(arr);
-const myn1 = arr.slice(1,3);
-console.log(myn1);
-console.log(arr);
-const myn2 = arr.splice(1,3);
-console.log(myn2);
-console.log(arr);
+// const arr = new Array(1, 2, 3, 4, 5);
+// console.log(arr);
+// const myn1 = arr.slice(1,3);
+// console.log(myn1);
+// console.log(arr);
+// const myn2 = arr.splice(1,3);
+// console.log(myn2);
+// console.log(arr);
+
+// const arr = new Array(1, 2, 3, 4, 5);
+// const arr1 = new Array(7, 8, 9, 5, 4);
+// // arr.push(arr1);
+// // console.log(arr);
+
+// const res = arr.concat(arr1);
+// console.log(res);
+
+// const res1 = [...arr, ...arr1];
+// console.log(res1);
+
+console.log(Array.from("Hemant"));
+console.log(Array.isArray("kumar"));
+const score1 = 100;
+const score2 = 200;
+const score3 = 300;
+console.log(Array.of(score1,score2, score3));
+
 
 
 
