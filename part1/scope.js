558da1db0c8a1550dad5f4523a8578dc1dce16b3
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-20 11:52:14 +0000

    Scope

---
+++ part1/scope.js
@@ -0,0 +1,21 @@
+// function one(){
+//     let username = "Hemant"
+//     function two(){
+//         const website ="youtube"
+//         console.log(username);
+//     }
+//    // console.log(website);
+//     two()
+// }
+// one();
+console.log(add(5));
+function add(num){
+     return num+1;
+}
+
+sum(8)
+const sum= function plus(num1){
+     return num1+1
+}
+
+
