376cdd93802f1918bbbe4ec6959635b87c24c2ad
Hemant <75944681+Hemant7239@users.noreply.github.com>
2023-10-21 09:51:08 +0000

    ifelse

---
+++ part2/1_control.js
@@ -0,0 +1,30 @@
+// const temp = 40;
+// if(temp < 30)
+// {
+//     console.log("temp is less than 30")
+// }
+// else{
+//     console.log("temp is greater than 30")
+// }
+// console.log("temp printed");
+
+//++++++++++++++++++++++ var vs let ++++++++++++++++++
+// const tm= 50;
+// if(tm<100)
+// {
+//     let pm ="fly";
+//     console.log(`let me : ${pm}`);
+// }
+// console.log(`let the ${pm}`);   // line is showing error 
+
+// const tm= 50;
+// if(tm<100)
+// {
+//     var pm ="fly";
+//     console.log(`let me : ${pm}`);
+// }
+// console.log(`let the ${pm}`);       // it not showing the error 
+
+
+ 
+
